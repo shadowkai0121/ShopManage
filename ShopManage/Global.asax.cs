231cd8c587431151831d3f790f77d12edc838f99
于 悠悠 <823yoga@gmail.com>
2019-06-13 21:05:10 +0800

    加入 logo頁面，登入完成導向index

--- ShopManage/Global.asax.cs
+++ ShopManage/Global.asax.cs
@@ -14,5 +14,10 @@ namespace ShopManage
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+        protected void Session_Start()
+        {
+            
+        }
     }
 }
